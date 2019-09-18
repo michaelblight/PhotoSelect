78053ef1d79ce280ffddd0a8974aafeccbf2d4db
BuildTools <unconfigured@null.spigotmc.org>
2019-09-18 23:31:45 +1000

    Showing folder list

--- PhotoSelect/Form1.Designer.cs
+++ PhotoSelect/Form1.Designer.cs
@@ -28,13 +28,275 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.selectAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 49);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 379);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.DrawMode = System.Windows.Forms.TreeViewDrawMode.OwnerDrawText;
+            this.treeView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.HideSelection = false;
+            this.treeView1.HotTracking = true;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(266, 379);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.TreeView1_BeforeExpand);
+            this.treeView1.DrawNode += new System.Windows.Forms.DrawTreeNodeEventHandler(this.TreeView1_DrawNode);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.OwnerDraw = true;
+            this.listView1.Size = new System.Drawing.Size(530, 379);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.DrawItem += new System.Windows.Forms.DrawListViewItemEventHandler(this.ListView1_DrawItem);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.recentToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripMenuItem.Image")));
+            this.openToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // recentToolStripMenuItem
+            // 
+            this.recentToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("recentToolStripMenuItem.Image")));
+            this.recentToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.recentToolStripMenuItem.Name = "recentToolStripMenuItem";
+            this.recentToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.recentToolStripMenuItem.Text = "Open &Recent";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(143, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cutToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.selectAllToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "&Edit";
+            // 
+            // cutToolStripMenuItem
+            // 
+            this.cutToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("cutToolStripMenuItem.Image")));
+            this.cutToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            this.cutToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.cutToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.cutToolStripMenuItem.Text = "Cu&t";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("copyToolStripMenuItem.Image")));
+            this.copyToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.copyToolStripMenuItem.Text = "&Copy";
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("pasteToolStripMenuItem.Image")));
+            this.pasteToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.pasteToolStripMenuItem.Text = "&Paste";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(141, 6);
+            // 
+            // selectAllToolStripMenuItem
+            // 
+            this.selectAllToolStripMenuItem.Name = "selectAllToolStripMenuItem";
+            this.selectAllToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.selectAllToolStripMenuItem.Text = "Select &All";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "&Tools";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.optionsToolStripMenuItem.Text = "&Options";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.aboutToolStripMenuItem.Text = "&About...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "PhotoSelect";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripMenuItem selectAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
--- PhotoSelect/Form1.cs
+++ PhotoSelect/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,221 @@ namespace PhotoSelect
 {
     public partial class Form1 : Form
     {
+        List<RecentFolder> recentFolders = new List<RecentFolder>();
+        RecentFolder currentFolder = null;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void FormDataSave()
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Settings.Default.WindowState = FormWindowState.Normal; // Don't save as minimized
+            } else {
+                Settings.Default.WindowState = this.WindowState;
+            }
+
+            if (this.WindowState == FormWindowState.Normal)
+                Settings.Default.WindowPosition = this.DesktopBounds; // Don't save if maximized/minimized
+
+            Settings.Default.SplitterPosition = splitContainer1.SplitterDistance;
+
+            Settings.Default.RecentFolders = ConvertToList(this.recentFolders);
+            Console.WriteLine("Saved " + Settings.Default.RecentFolders.Count.ToString() + " recent folders");
+
+            Settings.Default.Save();
+        }
+
+        private void FormDataRestore()
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.StartPosition = FormStartPosition.WindowsDefaultBounds;
+
+            if (Settings.Default.WindowPosition != Rectangle.Empty &&
+                IsVisibleOnAnyScreen(Settings.Default.WindowPosition))
+            {
+                this.StartPosition = FormStartPosition.Manual;
+                this.DesktopBounds = Settings.Default.WindowPosition;
+                this.WindowState = Settings.Default.WindowState;
+            }
+            else
+            {
+                this.StartPosition = FormStartPosition.WindowsDefaultLocation;
+            }
+
+            int distance = Settings.Default.SplitterPosition;
+            if (distance > 0)
+            {
+                splitContainer1.SplitterDistance = distance;
+            }
+
+            this.recentFolders = ConvertFromList(Settings.Default.RecentFolders as List<string>);
+            UpdateRecentFolders();
+            Console.WriteLine("Loaded " + recentFolders.Count.ToString() + " recent folders");
+        }
+
+        private bool IsVisibleOnAnyScreen(Rectangle rect)
+        {
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                if (screen.WorkingArea.IntersectsWith(rect))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string original, selected;
+
+            folderBrowserDialog1.Description = "First select the folder where your original photos are located";
+            if (this.folderBrowserDialog1.ShowDialog()  == DialogResult.OK)
+            {
+                original = folderBrowserDialog1.SelectedPath;
+                folderBrowserDialog1.Description = "Now select the folder where your selected photos are located";
+                if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    selected = folderBrowserDialog1.SelectedPath;
+                    if (original == selected) {
+                        MessageBox.Show("The original and selected folders cannot be the same", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    } else {
+                        RecentFolder r = new RecentFolder(original, selected);
+                        recentFolders.Add(r);
+                        OpenFolder(r);
+                    }
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            FormDataSave();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormDataRestore();
+        }
+
+        private void OpenFolder(RecentFolder folder) {
+            this.currentFolder = folder;
+            treeView1.Nodes.Clear();
+            var rootInfo = new DirectoryInfo(this.currentFolder.original);
+            var rootNode = treeView1.Nodes.Add(rootInfo.Name);
+            rootNode.Tag = rootInfo.FullName;
+            rootNode.ImageIndex = 0;
+            if (rootInfo.GetDirectories().First() != null) {
+                rootNode.Nodes.Add("Please wait...");
+            }
+        }
+
+        private void CreateChildNodes(TreeNode parentNode) {
+            var parentInfo = new DirectoryInfo((string)parentNode.Tag);
+            foreach (var directory in parentInfo
+                .GetDirectories()
+                .Where(d => d.Attributes.HasFlag(FileAttributes.Hidden) == false )
+                .OrderBy(d => d.Name)) {
+                var childPath = directory.FullName;
+                TreeNode childNode = parentNode.Nodes.Add(directory.Name);
+                childNode.Tag = childPath;
+                childNode.ImageIndex = 0;
+                var childInfo = new DirectoryInfo(childPath);
+                if (childInfo.GetDirectories().Length > 0) {
+                    childNode.Nodes.Add("Please wait...");
+                }
+            }
+        }
+
+        private void TreeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e) {
+            var expand = e.Node;
+            if (expand.ImageIndex == 0) {
+                expand.Nodes.Clear();
+                expand.ImageIndex = 1;
+                CreateChildNodes(expand);
+            }
+        }
+
+        private string GetPath(TreeNode node) {
+            List<string> s = new List<string>();
+            while (node.Parent != null) {
+                s.Append(node.Parent.Name);
+                node = node.Parent;
+            }
+            s.Reverse();
+            return string.Join(@"\", s) + @"\";
+;        }
+
+        private List<string> ConvertToList(List<RecentFolder> recentFolders) {
+            List<string> result = new List<string>();
+            recentFolders.ForEach(r => result.Add(r.original + "=" + r.selected));
+            return result;
+        }
+
+        private List<RecentFolder> ConvertFromList(List<string> recentFolders) {
+            List<RecentFolder> result = new List<RecentFolder>();
+            if (recentFolders == null) return result;
+
+            recentFolders.ForEach(r => {
+               string[] pair = r.Split(new[] { "=" }, StringSplitOptions.RemoveEmptyEntries);
+                if (pair.Length >= 2) result.Add(new RecentFolder(pair[0], pair[1]));
+            });
+            return result;
+        }
+
+        private void UpdateRecentFolders() {
+            recentToolStripMenuItem.DropDownItems.Clear();
+            this.recentFolders.ForEach(r => {
+                var item = recentToolStripMenuItem.DropDownItems.Add(r.original);
+                item.Click += RecentFolder_Click;
+            });
+        }
+
+        private void RecentFolder_Click(object sender, EventArgs e) {
+            var item = sender as ToolStripMenuItem;
+            foreach (RecentFolder r in this.recentFolders) {
+                if (r.original == item.Text) {
+                    OpenFolder(r);
+                    return;
+                }
+            }
+            Console.WriteLine("Couldn't find " + item.Text);
+        }
+
+        private void ListView1_DrawItem(object sender, DrawListViewItemEventArgs e) {
+
+        }
+
+        private void TreeView1_DrawNode(object sender, DrawTreeNodeEventArgs e) {
+            TreeNodeStates state = e.State;
+            Font font = e.Node.NodeFont ?? e.Node.TreeView.Font;
+            Color fore = e.Node.ForeColor;
+            Color back = SystemColors.Highlight;
+            if (fore == Color.Empty) fore = e.Node.TreeView.ForeColor;
+            if (e.Node == e.Node.TreeView.SelectedNode) {
+                fore = SystemColors.HighlightText;
+                e.Graphics.FillRectangle(new SolidBrush(back), e.Bounds);
+                ControlPaint.DrawFocusRectangle(e.Graphics, e.Bounds, fore, back);
+                TextRenderer.DrawText(e.Graphics, e.Node.Text, font, e.Bounds, fore, back, TextFormatFlags.GlyphOverhangPadding);
+            } else {
+                e.Graphics.FillRectangle(SystemBrushes.Window, e.Bounds);
+                TextRenderer.DrawText(e.Graphics, e.Node.Text, font, e.Bounds, fore, TextFormatFlags.GlyphOverhangPadding);
+            }
+        }
     }
+    public class RecentFolder {
+        public string original, selected;
+        public RecentFolder(string original, string selected) {
+            this.original = original;
+            this.selected = selected;
+        }
+    }
+
 }
